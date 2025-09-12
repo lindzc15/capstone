88dbdd173901b8ed1da5ed5e781f362117eea391
Lindzey Coonradt <lindzeycoonradt@mac.lan>
2025-09-12 12:29:50 -0600

    added about page/some styling

---
+++ frontend/src/pages/MainFooter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainFooter = () => {
+    return (
+        <footer className="text-center">
+            <p>&copy; {new Date().getFullYear()}<span className='logo-txt ms-2'>Let's Eat</span></p>
+        </footer>
+    );
+};
+
+export default MainFooter;
---
+++ frontend/src/pages/MainNav.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const MainNav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg sticky-top">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand" to="/">
+                    <img src="/logo.svg" alt="logo" width="80" height="60"></img>
+                    <span className="ms-2 navText logo-txt fw-normal">Let's Eat</span>
+                </NavLink>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse text-center" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link navText header-txt" to="/">Home</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default MainNav;
