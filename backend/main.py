b314e57c53b95cf6ee34bde2dd3cbf3bb5c5a26d
Lindzey Coonradt <lindzeycoonradt@dhcp-10-5-100-190.ad.uvu.edu>
2025-09-22 17:53:44 -0600

    added cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import login_controller, api_status
 from container import Container
 
 
 #create server
 app = FastAPI(title="CS 4900 Capstone", version="1.0.0")
+
+#add my frontend as an origin
+origins = [
+    "http://localhost:5173"
+]
+
+#allow my frontend full access to backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#add container for dependency injection
 container = Container()
 app.container = container
 
